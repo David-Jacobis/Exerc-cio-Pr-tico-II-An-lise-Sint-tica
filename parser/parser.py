98359e6c45a16f089464515f4feaef2fc14a439e
David-Jacobis <david.jacobis@outlook.com>
2022-11-27 11:44:36 -0300

    First commit

---
+++ parser/parser.py
@@ -0,0 +1,122 @@
+import sys
+
+from tag import Tag
+from token import Token
+from lexer import Lexer
+
+class Parser():
+
+   def __init__(self, lexer):
+      self.lexer = lexer
+      self.token = lexer.proxToken() # Leitura inicial obrigatoria do primeiro simbolo
+      if self.token is None: # erro no Lexer
+        sys.exit(0)
+
+   def sinalizaErroSintatico(self, message):
+      print("[Erro Sintatico] na linha " + str(self.token.getLinha()) + " e coluna " + str(self.token.getColuna()) + ": ")
+      print(message, "\n")
+      sys.exit(0)
+
+   def advance(self):
+      print("[DEBUG] token: ", self.token.toString())
+      self.token = self.lexer.proxToken()
+      if self.token is None: # erro no Lexer
+        sys.exit(0)
+
+   # verifica token esperado t 
+   def eat(self, t):
+      if(self.token.getNome() == t):
+         self.advance()
+         return True
+      else:
+         return False
+
+   """
+   ATENCAO:
+   Procure as marcacoes [TODO] para saber onde e o que fazer.
+   """
+
+   # Programa -> CMD EOF
+   def Programa(self):
+      self.Cmd()
+      if(self.token.getNome() != Tag.EOF):
+         self.sinalizaErroSintatico("Esperado \"EOF\"; encontrado " + "\"" + self.token.getLexema() + "\"")
+
+   def Cmd(self):
+      # Cmd -> if E then { CMD } CMD’
+      if(self.eat(Tag.KW_IF)): 
+         self.E()
+         if(not self.eat(Tag.KW_THEN)):
+            self.sinalizaErroSintatico("Esperado \"then\", encontrado " + "\"" + self.token.getLexema() + "\"")
+         if(not self.eat(Tag.SMB_AC)):
+            self.sinalizaErroSintatico("Esperado \"{\", encontrado " + "\"" + self.token.getLexema() + "\"")
+         self.Cmd()
+         if(not self.eat(Tag.SMB_FC)):
+            self.sinalizaErroSintatico("Esperado \"}\", encontrado " + "\"" + self.token.getLexema() + "\"")
+         self.CmdLinha()
+      # Cmd -> print T;
+      elif(self.eat(Tag.KW_PRINT)):
+        self.T()
+        if(not self.eat(Tag.SMB_PV)):
+            self.sinalizaErroSintatico("Esperado \";\", encontrado " + "\"" + self.token.getLexema() + "\"")
+      # Cmd -> ATRIB CMD
+      else:
+         self.Atrib()
+         self.Cmd()
+
+   def CmdLinha(self):
+      # CmdLinha -> else { CMD }
+      if(self.eat(Tag.KW_ELSE)):
+         if(not self.eat(Tag.SMB_AC)):
+            self.sinalizaErroSintatico("Esperado \"{\", encontrado " + "\"" + self.token.getLexema() + "\"")
+         self.Cmd()
+         if(not self.eat(Tag.SMB_FC)):
+            self.sinalizaErroSintatico("Esperado \"}\", encontrado " + "\"" + self.token.getLexema() + "\"")
+      # CmdLinha -> epsilon
+      else:
+         return
+
+   def Atrib(self):
+      # [TODO]: Implementar a logica para as producoes do nao terminal Atrib
+      # ATGENCAO: Lembre-se de sinalizar o erro com o(s) simbolo(s) esperado(s) e o 
+      # simbolo encontrado!
+      if(not(self.eat(Tag.ID) == self.T())):
+         self.sinalizaErroSintatico("Esperado \"id, terminal T\", encontrado " + "\"" + self.token.getLexema() + "\"")
+
+   
+
+   # E -> T E'
+   def E(self):
+      # [TODO]: Implementar a logica para as producoes do nao terminal E
+      # ATGENCAO: Lembre-se de sinalizar o erro com o(s) simbolo(s) esperado(s) e o 
+      # simbolo encontrado!
+      if(not(self.T())):
+         self.sinalizaErroSintatico("Esperado \"Não Terminal T'\", encontrado " + "\"" + self.token.getLexema() + "\"")
+         if(not(self.ELinha())):
+            self.sinalizaErroSintatico("Esperado \"Não Terminal E Linha'\", encontrado " + "\"" + self.token.getLexema() + "\"")
+
+
+
+   def ELinha(self):
+      # [TODO]: Implementar a logica para as producoes do nao terminal E'
+      # ATGENCAO: Lembre-se de sinalizar o erro com o(s) simbolo(s) esperado(s) e o 
+      # simbolo encontrado!
+      if(not(self.Op())):
+         self.sinalizaErroSintatico("Esperado \"Operadores\", encontrado " + "\"" + self.token.getLexema() + "\"")
+         if(not(self.T())):
+            self.sinalizaErroSintatico("Esperado \"numero, id\", encontrado " + "\"" + self.token.getLexema() + "\"")
+      else:
+         return
+      
+
+   # Op -> ">" | "<" | ">=" | "<=" | "==" | "!="
+   def Op(self):
+      if(not(
+         self.eat(Tag.OP_MAIOR) or self.eat(Tag.OP_MENOR) or self.eat(Tag.OP_MAIOR_IGUAL) or 
+         self.eat(Tag.OP_MENOR_IGUAL) or self.eat(Tag.OP_IGUAL) or self.eat(Tag.OP_DIFERENTE))):
+            self.sinalizaErroSintatico("Esperado \">, <, >=, <=, ==, !=\", encontrado " + "\"" + self.token.getLexema() + "\"")
+
+   # T -> id | num
+   def T(self):
+      if(not(self.eat(Tag.ID) or self.eat(Tag.NUM))):
+         self.sinalizaErroSintatico("Esperado \"numero, id\", encontrado " + "\"" + self.token.getLexema() + "\"")
